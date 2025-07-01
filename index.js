9fb209bc482450607d54f117bd2005860fd4dcad
Joan Chepkoech <joan92311@gmail.com>
2025-07-01 05:54:48 +0300

    complete-phase-1-sending-data-with-fetch

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Add your code here
+function submitData(name, email) {
+  return fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify({
+      name: name,
+      email: email
+    })
+  })
+    .then(response => response.json())
+    .then(user => {
+      const p = document.createElement("p");
+      p.textContent = user.id;
+      document.body.appendChild(p);
+    })
+    .catch(error => {
+      const p = document.createElement("p");
+      p.textContent = error.message;
+      document.body.appendChild(p);
+    });
+}
+
